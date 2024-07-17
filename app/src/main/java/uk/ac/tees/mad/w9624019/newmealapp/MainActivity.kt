db3113c32554e69fd783b320ffc6353ff508e2c9
Anusha <w9624019@live.tees.ac.uk>
2024-07-17 07:46:51 +0100

    NewMeal app has been created.

---
+++ app/src/main/java/uk/ac/tees/mad/w9624019/newmealapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package uk.ac.tees.mad.w9624019.newmealapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uk.ac.tees.mad.w9624019.newmealapp.ui.theme.NewMealAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewMealAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Welcome to new MealApp")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewMealAppTheme {
+        Greeting("Android")
+    }
+}
